9b8a79da978506898fe080fea1b33a9049c6ddd7
patricklathrop <plathrop69@gmail.com>
2019-12-14 17:22:04 -0800

    step 1 js code complete - correct strict syntax

--- js/app.js
+++ js/app.js
@@ -4,8 +4,13 @@ console.log('js online');
 // global variable cookieArray will stor the objects pushed to it in the format cookieArray[#].<object> //
 var cookieArray = [];
 var shopHour = 14;
+var totalCookiesSold = [];
 
-// //
+for (var i=0; i < shopHour; i++){
+  totalCookiesSold.push(0);
+}
+
+
 /**
  * this function is used to bundle parameters passed to it, as well as adding an empty array called 'cookiesPerHour' that will store the number of 'cookies per hour', and returning an object.
  * @param {string} location this is the city location
@@ -13,23 +18,51 @@ var shopHour = 14;
  * @param {number} maxCust this is the maximum customers per hour
  * @param {number} avgCookieSale average cookies per customer per hour
  */
-function newPlace(location, minCust, maxCust, avgCookieSale){
+function NewPlace(location, minCust, maxCust, avgCookieSale){
   this.location = location;
   this.minCust = minCust;
   this.maxCust = maxCust;
   this.avgCookieSale = avgCookieSale;
   this.cookiesPerHour = [];
+  this.totalCookies = 0;
 }
 
 /**
  * each line below is calling 'function newPlace' and passing along the parameters listed to bundle into an object that can be passed to cookieArray
  */
 function coreLocation() {
-cookieArray.push(new newPlace("Seattle", 23, 65, 6.3));
-cookieArray.push(new newPlace("Tokyo", 3, 24, 1.2));
-cookieArray.push(new newPlace("Dubai", 11, 38, 3.7));
-cookieArray.push(new newPlace("Paris", 20, 38, 2.3));
-cookieArray.push(new newPlace("Lima", 2, 16, 4.6));
+  cookieArray.push(new NewPlace('Seattle', 23, 65, 6.3));
+  cookieArray.push(new NewPlace('Tokyo', 3, 24, 1.2));
+  cookieArray.push(new NewPlace('Dubai', 11, 38, 3.7));
+  cookieArray.push(new NewPlace('Paris', 20, 38, 2.3));
+  cookieArray.push(new NewPlace('Lima', 2, 16, 4.6));
+}
+coreLocation();
+
+/** function is called to populate the cookiesPerHour array */
+function calcSales (){
+  for (i=0; i < cookieArray.length; i++){
+    for (var j=0; j < shopHour; j++){
+      var n=randomNum( cookieArray[i].minCust, cookieArray[i].maxCust) * cookieArray[i].avgCookieSale;
+
+      cookieArray[i].cookiesPerHour.push(n);
+
+      cookieArray[i].totalCookies+=n;
+
+      totalCookiesSold[j]+=n;
+
+    }
+  }
+}
+calcSales;
+
+
+
+
+/** number randomizer */
+function randomNum(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+
 }
 
 
